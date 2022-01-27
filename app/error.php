61c3686888d338af3ccab44eca43bec4995740f8
nadahelene <helenepdy@gmail.com>
2022-01-27 16:38:16 +0100

    ajout de webapp

---
+++ app/error.php
@@ -0,0 +1,26 @@
+<?php
+
+// Désactiver le rapport d'erreurs
+error_reporting(0);
+
+// Rapporte les erreurs d'exécution de script
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+// Rapporter les E_NOTICE peut vous aider à améliorer vos scripts
+// (variables non initialisées, variables mal orthographiées..)
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+// Rapporte toutes les erreurs à part les E_NOTICE
+// C'est la configuration par défaut de php.ini
+error_reporting(E_ALL & ~E_NOTICE);
+
+// Rapporte toutes les erreurs PHP
+error_reporting(E_ALL);
+
+// Rapporte toutes les erreurs PHP
+error_reporting(-1);
+
+// Même chose que error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+?>
